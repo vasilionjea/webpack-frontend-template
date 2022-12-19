4e9a8f53033c022350148eb6cfc57d825920fa2a
Billy Onjea <vasili.onjea@gmail.com>
2022-12-19 02:36:20 +0200

    add link to Jest configuration docs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// For a detailed explanation of each configuration property visit:
+// https://jestjs.io/docs/configuration
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
